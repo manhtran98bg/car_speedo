e4553608cbf56d179371658496b3d1e7679bf749
Trần Mạnh <manhtran98bg@gmail.com>
2025-09-17 17:58:03 +0700

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,330 +1,51 @@
-// #include <Arduino.h>
-// #include "Views/main_view.h"
-// #include "Drivers/screen_driver.h"
-// #include "Arduino_DriveBus_Library.h"
-// #include <AnimatedGIF.h>
-// #include "SD.h"
-
-// extern void gif_splash_view_init();
-// void setup()
-// {
-//   delay(2000);
-//   Serial.begin(115200);
-//   // display_init();
-//   // gif_splash_view_init();
-//   // main_view_init();
-
-//   // test sdcard
-//   int attempts = 0;
-//   int maxAttempts = 50;
-//   int delayBetweenAttempts = 300;
-//   bool isblinked = false;
-//   pinMode(GPIO_NUM_40, OUTPUT);
-//   SPI.begin(GPIO_NUM_41, GPIO_NUM_48, GPIO_NUM_47, GPIO_NUM_40);
-//   while (!SD.begin(GPIO_NUM_40, SPI))
-//   {
-//     Serial.printf("SD Card mount failed! (attempt %d of %d)\r\n", attempts, maxAttempts);
-//     isblinked = !isblinked;
-//     attempts++;
-//     // if (isblinked)
-//     // {
-//     //   tft.setTextColor(TFT_WHITE, TFT_BLACK);
-//     // }
-//     // else
-//     // {
-//     //   tft.setTextColor(TFT_BLACK, TFT_WHITE);
-//     // }
-//     // tft.drawString("INSERT SD", tft.width() / 2, tft.height() / 2);
-
-//     if (attempts > maxAttempts)
-//     {
-//       Serial.println("Giving up");
-//     }
-//     delay(delayBetweenAttempts);
-//   }
-// }
-
-// void loop()
-// {
-// }
-
-/*
- * Connect the SD card to the following pins:
- *
- * SD Card | ESP32
- *    D2       -
- *    D3       SS
- *    CMD      MOSI
- *    VSS      GND
- *    VDD      3.3V
- *    CLK      SCK
- *    VSS      GND
- *    D0       MISO
- *    D1       -
- */
-#include "FS.h"
+#include <Arduino.h>
+#include "Views/main_view.h"
+#include "Drivers/screen_driver.h"
+#include "Arduino_DriveBus_Library.h"
+#include <AnimatedGIF.h>
 #include "SD.h"
-#include "SPI.h"
 
-void listDir(fs::FS &fs, const char *dirname, uint8_t levels)
-{
-  Serial.printf("Listing directory: %s\n", dirname);
-
-  File root = fs.open(dirname);
-  if (!root)
-  {
-    Serial.println("Failed to open directory");
-    return;
-  }
-  if (!root.isDirectory())
-  {
-    Serial.println("Not a directory");
-    return;
-  }
-
-  File file = root.openNextFile();
-  while (file)
-  {
-    if (file.isDirectory())
-    {
-      Serial.print("  DIR : ");
-      Serial.println(file.name());
-      if (levels)
-      {
-        listDir(fs, file.path(), levels - 1);
-      }
-    }
-    else
-    {
-      Serial.print("  FILE: ");
-      Serial.print(file.name());
-      Serial.print("  SIZE: ");
-      Serial.println(file.size());
-    }
-    file = root.openNextFile();
-  }
-}
-
-void createDir(fs::FS &fs, const char *path)
-{
-  Serial.printf("Creating Dir: %s\n", path);
-  if (fs.mkdir(path))
-  {
-    Serial.println("Dir created");
-  }
-  else
-  {
-    Serial.println("mkdir failed");
-  }
-}
-
-void removeDir(fs::FS &fs, const char *path)
-{
-  Serial.printf("Removing Dir: %s\n", path);
-  if (fs.rmdir(path))
-  {
-    Serial.println("Dir removed");
-  }
-  else
-  {
-    Serial.println("rmdir failed");
-  }
-}
-
-void readFile(fs::FS &fs, const char *path)
-{
-  Serial.printf("Reading file: %s\n", path);
-
-  File file = fs.open(path);
-  if (!file)
-  {
-    Serial.println("Failed to open file for reading");
-    return;
-  }
-
-  Serial.print("Read from file: ");
-  while (file.available())
-  {
-    Serial.write(file.read());
-  }
-  file.close();
-}
-
-void writeFile(fs::FS &fs, const char *path, const char *message)
-{
-  Serial.printf("Writing file: %s\n", path);
-
-  File file = fs.open(path, FILE_WRITE);
-  if (!file)
-  {
-    Serial.println("Failed to open file for writing");
-    return;
-  }
-  if (file.print(message))
-  {
-    Serial.println("File written");
-  }
-  else
-  {
-    Serial.println("Write failed");
-  }
-  file.close();
-}
-
-void appendFile(fs::FS &fs, const char *path, const char *message)
-{
-  Serial.printf("Appending to file: %s\n", path);
-
-  File file = fs.open(path, FILE_APPEND);
-  if (!file)
-  {
-    Serial.println("Failed to open file for appending");
-    return;
-  }
-  if (file.print(message))
-  {
-    Serial.println("Message appended");
-  }
-  else
-  {
-    Serial.println("Append failed");
-  }
-  file.close();
-}
-
-void renameFile(fs::FS &fs, const char *path1, const char *path2)
-{
-  Serial.printf("Renaming file %s to %s\n", path1, path2);
-  if (fs.rename(path1, path2))
-  {
-    Serial.println("File renamed");
-  }
-  else
-  {
-    Serial.println("Rename failed");
-  }
-}
-
-void deleteFile(fs::FS &fs, const char *path)
-{
-  Serial.printf("Deleting file: %s\n", path);
-  if (fs.remove(path))
-  {
-    Serial.println("File deleted");
-  }
-  else
-  {
-    Serial.println("Delete failed");
-  }
-}
-
-void testFileIO(fs::FS &fs, const char *path)
-{
-  File file = fs.open(path);
-  static uint8_t buf[512];
-  size_t len = 0;
-  uint32_t start = millis();
-  uint32_t end = start;
-  if (file)
-  {
-    len = file.size();
-    size_t flen = len;
-    start = millis();
-    while (len)
-    {
-      size_t toRead = len;
-      if (toRead > 512)
-      {
-        toRead = 512;
-      }
-      file.read(buf, toRead);
-      len -= toRead;
-    }
-    end = millis() - start;
-    Serial.printf("%u bytes read for %u ms\n", flen, end);
-    file.close();
-  }
-  else
-  {
-    Serial.println("Failed to open file for reading");
-  }
-
-  file = fs.open(path, FILE_WRITE);
-  if (!file)
-  {
-    Serial.println("Failed to open file for writing");
-    return;
-  }
-
-  size_t i;
-  start = millis();
-  for (i = 0; i < 2048; i++)
-  {
-    file.write(buf, 512);
-  }
-  end = millis() - start;
-  Serial.printf("%u bytes written for %u ms\n", 2048 * 512, end);
-  file.close();
-}
-
+extern void gif_splash_view_init();
 void setup()
 {
+  delay(2000);
   Serial.begin(115200);
-  pinMode(40, INPUT_PULLUP);
-  delay(10);
-  SPI.begin(41, 48, 47, 40);
-  SPI.setDataMode(SPI_MODE0);
-  delay(10);
-  if (!SD.begin(40, SPI, 8000000)) // 1-bit SD bus mode
-  {
-    Serial.println(F("ERROR: SD card mount failed!"));
-    return ;
-  }
-    uint8_t cardType = SD.cardType();
+  display_init();
+  gif_splash_view_init();
+  main_view_init();
 
-    if (cardType == CARD_NONE)
-    {
-      Serial.println("No SD card attached");
-      return;
-    }
+  // test sdcard
+  // int attempts = 0;
+  // int maxAttempts = 50;
+  // int delayBetweenAttempts = 300;
+  // bool isblinked = false;
+  // pinMode(GPIO_NUM_40, OUTPUT);
+  // SPI.begin(GPIO_NUM_41, GPIO_NUM_48, GPIO_NUM_47, GPIO_NUM_40);
+  // while (!SD.begin(GPIO_NUM_40, SPI))
+  // {
+  //   Serial.printf("SD Card mount failed! (attempt %d of %d)\r\n", attempts, maxAttempts);
+  //   isblinked = !isblinked;
+  //   attempts++;
+  //   // if (isblinked)
+  //   // {
+  //   //   tft.setTextColor(TFT_WHITE, TFT_BLACK);
+  //   // }
+  //   // else
+  //   // {
+  //   //   tft.setTextColor(TFT_BLACK, TFT_WHITE);
+  //   // }
+  //   // tft.drawString("INSERT SD", tft.width() / 2, tft.height() / 2);
 
-    Serial.print("SD Card Type: ");
-    if (cardType == CARD_MMC)
-    {
-      Serial.println("MMC");
-    }
-    else if (cardType == CARD_SD)
-    {
-      Serial.println("SDSC");
-    }
-    else if (cardType == CARD_SDHC)
-    {
-      Serial.println("SDHC");
-    }
-    else
-    {
-      Serial.println("UNKNOWN");
-    }
+  //   if (attempts > maxAttempts)
+  //   {
+  //     Serial.println("Giving up");
+  //   }
+  //   delay(delayBetweenAttempts);
+  // }
+}
 
-    uint64_t cardSize = SD.cardSize() / (1024 * 1024);
-    Serial.printf("SD Card Size: %lluMB\n", cardSize);
+void loop()
+{
+}
 
-    listDir(SD, "/", 0);
-    createDir(SD, "/mydir");
-    listDir(SD, "/", 0);
-    removeDir(SD, "/mydir");
-    listDir(SD, "/", 2);
-    writeFile(SD, "/hello.txt", "Hello ");
-    appendFile(SD, "/hello.txt", "World!\n");
-    readFile(SD, "/hello.txt");
-    deleteFile(SD, "/foo.txt");
-    renameFile(SD, "/hello.txt", "/foo.txt");
-    readFile(SD, "/foo.txt");
-    testFileIO(SD, "/test.txt");
-    Serial.printf("Total space: %lluMB\n", SD.totalBytes() / (1024 * 1024));
-    Serial.printf("Used space: %lluMB\n", SD.usedBytes() / (1024 * 1024));
-  }
 
-  void loop()
-  {
-  }
